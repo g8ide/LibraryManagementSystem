b16de3a61fa522f3a6b57789a723125b942e1dc3
g8ide <saqlain.walayat@gmail.com>
2026-06-12 08:17:57 +0500

    Update BookModule.py

--- BookModule.py
+++ BookModule.py
@@ -1,1 +1,16 @@
+books = []
 
+def add_book():
+    bid = input("Book ID: ")
+    title = input("Book Title: ")
+
+    books.append({
+        "id": bid,
+        "title": title
+    })
+
+    print("Book Added")
+
+def view_books():
+    for b in books:
+        print(b)
