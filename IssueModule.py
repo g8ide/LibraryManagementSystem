06fae7304c1518fa72cd890a02958d2a8b2e900f
g8ide <saqlain.walayat@gmail.com>
2026-06-12 08:18:17 +0500

    Update IssueModule.py

--- IssueModule.py
+++ IssueModule.py
@@ -1,1 +1,16 @@
+issued_books = []
 
+def issue_book():
+    sid = input("Student ID: ")
+    bid = input("Book ID: ")
+
+    issued_books.append({
+        "student": sid,
+        "book": bid
+    })
+
+    print("Book Issued")
+
+def view_issued():
+    for item in issued_books:
+        print(item)
