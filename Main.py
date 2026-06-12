7bc331bfb6a6e7d4c7ad3803e9adf8b28c9fe029
g8ide <saqlain.walayat@gmail.com>
2026-06-12 08:19:05 +0500

    Add library management system functionality
    Implemented a basic library management system with options to add/view students and books, and issue books.

--- Main.py
+++ Main.py
@@ -1,1 +1,39 @@
+from StudentModule import *
+from BookModule import *
+from IssueModule import *
 
+while True:
+    print("\n===== Library Management System =====")
+    print("1. Add Student")
+    print("2. View Students")
+    print("3. Add Book")
+    print("4. View Books")
+    print("5. Issue Book")
+    print("6. View Issued Books")
+    print("7. Exit")
+
+    choice = input("Enter Choice: ")
+
+    if choice == "1":
+        add_student()
+
+    elif choice == "2":
+        view_students()
+
+    elif choice == "3":
+        add_book()
+
+    elif choice == "4":
+        view_books()
+
+    elif choice == "5":
+        issue_book()
+
+    elif choice == "6":
+        view_issued()
+
+    elif choice == "7":
+        break
+
+    else:
+        print("Invalid Choice")
