261612d92cd8f77415f0aac8224665c98e7e8a82
g8ide <saqlain.walayat@gmail.com>
2026-06-12 08:17:38 +0500

    Update StudentModule.py

--- StudentModule.py
+++ StudentModule.py
@@ -1,1 +1,16 @@
+students = []
 
+def add_student():
+    sid = input("Enter Student ID: ")
+    name = input("Enter Student Name: ")
+
+    students.append({
+        "id": sid,
+        "name": name
+    })
+
+    print("Student Added Successfully")
+
+def view_students():
+    for s in students:
+        print(s)
